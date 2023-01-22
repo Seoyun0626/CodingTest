9d39fc22374354fccd81acb04d92df63948f4d11
Seoyun0626 <104416283+Seoyun0626@users.noreply.github.com>
2023-01-22 15:16:36 +0900

    Create (BOJ)1021_회전하는 큐.py

---
+++ 요목조목/(BOJ)1021_회전하는 큐.py
@@ -0,0 +1,56 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+queue = deque()
+
+
+def first_command():
+    queue.popleft()
+
+
+def second_command():
+    queue.append(queue.popleft())
+
+
+def third_command():
+    queue.appendleft(queue.pop())
+
+
+N, M = map(int, input().split())
+for i in range(1, N + 1):
+    queue.append(i)
+
+
+arr = list(map(int, input().split()))
+cnt = 0
+
+
+for i in range(M):
+    if queue[0] == arr[i]:
+        first_command()
+    else:
+        if (len(queue) / 2) > (queue.index(arr[i])):
+            while arr[i] != queue[0]:
+                second_command()
+                cnt += 1
+                if queue[0] == arr[i]:
+                    first_command()
+                    break
+        else:
+            while arr[i] != queue[0]:
+                third_command()
+                cnt += 1
+                if queue[0] == arr[i]:
+                    first_command()
+                    break
+print(cnt)
+
+
+
+
+
+
+
+
+
+
