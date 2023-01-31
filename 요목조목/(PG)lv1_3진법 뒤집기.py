336c9dd2d4ea55eed0fe2a9acb07183385cdbe8d
Seoyun0626 <104416283+Seoyun0626@users.noreply.github.com>
2023-01-31 10:56:22 +0900

    Create (PG)lv1_3진법 뒤집기.py

---
+++ 요목조목/(PG)lv1_3진법 뒤집기.py
@@ -0,0 +1,11 @@
+n = int(input())
+string = ""
+while (n != 0):
+    string += str(n % 3)
+    n = n // 3
+result = 0
+string = string[::-1]
+
+for i in range(len(string)):
+    result += (3 ** i) * int(string[i])
+print(result)
