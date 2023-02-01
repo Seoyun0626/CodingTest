817f51d2248dde13445128e5c3d34e7c4fe9dd4d
Seoyun0626 <104416283+Seoyun0626@users.noreply.github.com>
2023-02-01 10:49:23 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -0,0 +1,24 @@
+def solution(N, stages):
+    dict = {}
+    result = []
+    length = len(stages)
+    i = 1
+    while i <= N:
+        cnt = 0
+        for j in range(len(stages)):
+            if stages[j] == i:
+                cnt += 1
+        if length == 0:
+            dict[i] = 0
+        else:
+            dict[i] = cnt / length
+            length = length - cnt
+        i += 1
+
+
+    new_dict = sorted(dict.items(), key = lambda x : x[1], reverse = True)
+
+
+    for i in range(len(new_dict)):
+        result.append(new_dict[i][0])
+    return result
