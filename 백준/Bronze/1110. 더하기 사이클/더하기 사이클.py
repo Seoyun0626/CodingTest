0d1e04fcd61e7d60cd37c22cd706c363a47e6f52
Seoyun0626 <104416283+Seoyun0626@users.noreply.github.com>
2023-02-03 23:33:05 +0900

    [Bronze I] Title: 더하기 사이클, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,15 +1,26 @@
-n = int(input()) 
-num = n
-count = 0
+N = input()
+cnt = 0
+NEW_N = ""
+flag = 0
 
-while True:
-    a = num//10 
-    b = num%10 
-    c = (a+b)%10
-    num = (b*10)+c 
-    
-    count+=1
-    if(num==n):
-        break
-
-print(count)
+while N != NEW_N:
+    if flag == 0:
+        if len(N) == 1:
+            N = "0" + N
+            total = str(int(N[0]) + int(N[1]))
+            NEW_N = N[-1] + total[-1]
+            flag = 1
+        else:
+            total = str(int(N[0]) + int(N[1]))
+            NEW_N = N[-1] + total[-1]
+            flag = 1
+    else:
+        if len(NEW_N) == 1:
+            NEW_N = "0" + N
+            total = str(int(NEW_N[0]) + int(NEW_N[1]))
+            NEW_N = NEW_N[-1] + total[-1]
+        else:
+            total = str(int(NEW_N[0]) + int(NEW_N[1]))
+            NEW_N = NEW_N[-1] + total[-1]
+    cnt += 1
+print(cnt)
