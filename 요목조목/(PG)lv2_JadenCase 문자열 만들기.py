6271c826b3569cf3bfb42cb9db7f4db9a23f605b
Seoyun0626 <tjdbs0626@gmail.com>
2023-02-07 14:51:57 +0900

    Create (PG)lv2_JadenCase 문자열 만들기.py

---
+++ 요목조목/(PG)lv2_JadenCase 문자열 만들기.py
@@ -0,0 +1,45 @@
+s = "3people unFollowed  me     "
+result = ""
+i = 0
+flag = 0
+while True:
+    # print(s[i], len(s))
+    if s[i].islower():
+        result += s[i].upper()
+    else:
+        result += s[i]
+    i += 1
+    # print(result, s[i], i, len(s))
+    if i == len(s):
+        print(result)
+        break
+    while s[i] != " ":
+        if s[i].isupper():
+            result += s[i].lower()
+        else:
+            result += s[i]
+        i += 1
+        if i == len(s):
+            flag = 1
+            break
+        # print(result, i, s[i], flag, len(s))
+    if flag == 1:
+        print(result)
+        break
+    # print(result)
+    # print(result, i, s[i], len(s))
+    while s[i] == " ":
+        result += s[i]
+        i += 1
+        if len(s) == i:
+            flag = 1
+            break
+    if flag == 1:
+        print(result)
+        break
+
+
+
+
+
+
