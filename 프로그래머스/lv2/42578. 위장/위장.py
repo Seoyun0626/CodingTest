b4524c1c61b045bef35175ca4a33d1428b0b6fd1
Seoyun0626 <104416283+Seoyun0626@users.noreply.github.com>
2023-02-09 15:43:24 +0900

    [level 2] Title: 위장, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -0,0 +1,17 @@
+def solution(clothes):
+    # 입거나 안 입거나
+    # clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
+    dic = {}
+    answer = 1
+
+
+    for item, category in clothes:
+        if category not in dic:
+            dic[category] = 1
+        else:
+            dic[category] += 1
+
+    for key, value in dic.items():
+        answer *= (value + 1)
+    answer -= 1
+    return answer
