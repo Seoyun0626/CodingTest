aaaebe54b0dfd717ec2a626ce332cdb3ba6eabce
Seoyun0626 <tjdbs0626@gmail.com>
2023-02-10 11:00:56 +0900

    Create (PG)lv2_전화번호 목록.py

---
+++ 요목조목/(PG)lv2_전화번호 목록.py
@@ -0,0 +1,23 @@
+# phone_book = ["119", "97674223", "1195524421"]
+# phone_book = ["123","456","789"]
+phone_book = ["12","123","1235","567","88"]
+answer = True
+dic = {}
+
+for x in phone_book:
+    dic[x] = 1
+print(dic)
+
+for check in phone_book:
+    num = ""
+    for y in check:
+        num += y
+        if num in dic and num != check:
+            answer = False
+            break
+    if answer == False:
+        break
+print(answer)
+
+
+
