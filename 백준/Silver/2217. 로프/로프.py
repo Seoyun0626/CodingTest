1070a418493cff1e9c9d7d3a429573aee0f58c24
Seoyun0626 <104416283+Seoyun0626@users.noreply.github.com>
2023-02-11 20:49:32 +0900

    [Silver IV] Title: 로프, Time: 112 ms, Memory: 35500 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -8,11 +8,10 @@ for _ in range(n):
     nl.append(int(input()))
 nl.sort(reverse=True)
 max_num = 0
-
 for i in range(0, n):
     sum_tmp = nl[i] * (i + 1)
     if sum_tmp >= max_num:
         max_num = sum_tmp
-
+ 
 
 print(max_num)
