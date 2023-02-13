4afb1232554bb7c2aa68cd9c505f5419123a024b
Seoyun0626 <tjdbs0626@gmail.com>
2023-02-13 15:49:21 +0900

    Create (PG)lv2_더 맵게.py

---
+++ 요목조목/(PG)lv2_더 맵게.py
@@ -0,0 +1,23 @@
+import heapq
+
+
+scoville = [1, 2, 3, 9, 10, 12]
+heapq.heapify(scoville)
+K = 7
+result = 0
+
+while True:
+    first = heapq.heappop(scoville)
+    if first >= K:
+        break
+    else:
+        if len(scoville) == 0:
+            result = -1
+            break
+        else:
+            second = heapq.heappop(scoville)
+            new = first + 2 * second
+            heapq.heappush(scoville, new)
+            result += 1
+print(result)
+
