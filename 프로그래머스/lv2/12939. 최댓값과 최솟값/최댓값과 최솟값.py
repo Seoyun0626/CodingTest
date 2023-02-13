0de4da8a07c0bfd3a2ba99154abc5068a8ca8f4d
Seoyun0626 <104416283+Seoyun0626@users.noreply.github.com>
2023-02-13 16:29:28 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -0,0 +1,17 @@
+def solution(s):
+    # s = "1 2 3 4"
+    # s = "-1 -2 -3 -4"
+    # s = "-1 -1"
+    answer = ""
+    tmp = list(map(int, s.split( )))
+    result = []
+    result.append(min(tmp))
+    result.append(max(tmp))
+    print(result)
+
+    for x in result:
+        answer += str(x)
+        answer += " "
+    answer = answer.rstrip(" ")
+
+    return answer
