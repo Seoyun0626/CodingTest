f54c41e2f8ad6c778ede1034382b8a1d8d20dbe2
Seoyun0626 <tjdbs0626@gmail.com>
2023-02-15 15:33:10 +0900

    Update (BOJ)1541_잃어버린 괄호.py

--- 요목조목/(BOJ)1541_잃어버린 괄호.py
+++ 요목조목/(BOJ)1541_잃어버린 괄호.py
@@ -1,39 +1,50 @@
-import sys
+# import sys
+#
+# input = sys.stdin.readline
+# s = input()
+# arr = []
+# result = 0
+# string = ""
+# for i in range(len(s)):
+#     if s[i] != "-" and s[i] != "+":
+#         string += s[i]
+#     else:
+#         arr.append(string)
+#         arr.append(s[i])
+#         string = ""
+# arr.append(string.strip("\n"))
+# # print(arr)
+# flag = 0
+# i = 0
+# while True:
+#     if arr[i].isnumeric():
+#         result += int(arr[i])
+#     elif arr[i] == "-":
+#         i += 1
+#         while arr[i] != "-":
+#             if arr[i].isnumeric():
+#                 result -= int(arr[i])
+#             i += 1
+#             if i == len(arr):
+#                 flag = 1
+#                 break
+#         i -= 1
+#     if flag:
+#         break
+#     i += 1
+#     if len(arr) == i:
+#         break
+# print(result)
+#
+#
 
-input = sys.stdin.readline
-s = input()
-arr = []
-result = 0
-string = ""
-for i in range(len(s)):
-    if s[i] != "-" and s[i] != "+":
-        string += s[i]
-    else:
-        arr.append(string)
-        arr.append(s[i])
-        string = ""
-arr.append(string.strip("\n"))
-# print(arr)
-flag = 0
-i = 0
-while True:
-    if arr[i].isnumeric():
-        result += int(arr[i])
-    elif arr[i] == "-":
-        i += 1
-        while arr[i] != "-":
-            if arr[i].isnumeric():
-                result -= int(arr[i])
-            i += 1
-            if i == len(arr):
-                flag = 1
-                break
-        i -= 1
-    if flag:
-        break
-    i += 1
-    if len(arr) == i:
-        break
-print(result)
-
+arr = input().split("-")
+s = 0
+print(arr)
+for i in arr[0].split("+"):
+    s += int(i)
+for i in arr[1:]:
+    for j in i.split("+"):
+        s -= int(j)
+print(s)
 
