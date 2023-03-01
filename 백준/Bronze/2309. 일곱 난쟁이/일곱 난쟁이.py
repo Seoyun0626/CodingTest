d3c2e5e4fc9d08b5a2e6f091469f1a076056f76b
Seoyun0626 <104416283+Seoyun0626@users.noreply.github.com>
2023-03-01 22:59:30 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,23 @@
+import sys
+
+heights = []
+for i in range(9):
+    heights.append(int(sys.stdin.readline())) 
+
+total = sum(heights)
+one = 0
+two = 0
+
+for i in range(9):
+    for j in range(i+1, 9):
+        if total - (heights[i] + heights[j]) == 100:
+            one, two = heights[i], heights[j]
+            break
+
+heights.remove(one)
+heights.remove(two)
+heights.sort() 
+
+
+for i in heights:
+    print(i)
