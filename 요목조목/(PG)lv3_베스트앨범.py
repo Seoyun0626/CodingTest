7e013a04118ca7062a2c72d1a448a5f6d811b425
Seoyun0626 <tjdbs0626@gmail.com>
2023-09-04 13:57:59 +0900

    Code 베스트앨범.py
    Code 베스트앨범.py

---
+++ 요목조목/(PG)lv3_베스트앨범.py
@@ -0,0 +1,31 @@
+dic = {} #장르 + 횟수
+total_play = {} # 장르별 총 재생횟수
+answer = []
+
+genres = ["classic", "pop", "classic", "classic", "pop"]
+plays = [500, 600, 150, 800, 2500]
+for i in range (len(genres)):
+    genre = genres[i]
+    play = plays[i]
+    if genre not in dic.keys():
+        dic[genre] = [(play, i)]
+        total_play[genre] = play
+    else:
+        total_play[genre] += play
+        dic[genre].append((play, i))
+print(dic)
+# print(total_play)
+
+total_play = sorted(total_play.items(), key = lambda x : x[1], reverse=True)
+print(total_play)
+
+for key in total_play:
+    playlist = dic[key[0]]
+    playlist = sorted(playlist, key = lambda x : x[0], reverse = True)
+    print(playlist)
+    if len(playlist) == 1:
+        answer.append(playlist[0][1])
+    else:
+        for i in range(2):
+            answer.append(playlist[i][1])
+print(answer)
