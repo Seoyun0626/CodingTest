8dfe23192d3a0e0d5c34fa56127299b3aae39022
Seoyun0626 <104416283+Seoyun0626@users.noreply.github.com>
2023-09-14 15:19:01 +0900

    [level 1] Title: K번째수, Time: 0.23 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,6 +1,25 @@
+
+
+import java.util.*;
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
+    public int[] solution(int[] array, int[][] commands)
+    {
+        int[] answer = new int[commands.length];
+        // array배열을 자른 list
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < commands.length; i++)
+        {
+            // array 숫자 배열 자르기
+            for (int j = commands[i][0]; j <= commands[i][1]; j++)
+            {
+                list.add(array[j-1]);
+            }
+            // 오름차순 정렬
+            Collections.sort(list);
+            answer[i] = list.get(commands[i][2] - 1);
+            list.clear();
+        }
         return answer;
     }
 }
