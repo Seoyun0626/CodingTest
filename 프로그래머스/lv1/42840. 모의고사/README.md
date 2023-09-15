30832231c823ccb93209fd1be88f279626aae9bb
Seoyun0626 <104416283+Seoyun0626@users.noreply.github.com>
2023-09-15 09:17:27 +0900

    [level 1] Title: 모의고사, Time: 3.15 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/README.md
+++ 프로그래머스/lv1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 8.76 ms
+메모리: 10.3 MB, 시간: 3.15 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,4 +1,7 @@
 def solution(answers):
+    # answers = [1,2,3,4,5]
+    # answers = [1,3,2,4,2]
+
     person1 = [1, 2, 3, 4, 5]
     person2 = [2, 1, 2, 3, 2, 4, 2, 5]
     person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
@@ -10,22 +13,22 @@ def solution(answers):
     cnt1 = 0
     cnt2 = 0
     cnt3 = 0
+
     result = []
 
     for i in range(len(answers)):
-        an_person1.append(person1[i % len(person1)])
-        an_person2.append(person2[i % len(person2)])
-        an_person3.append(person3[i % len(person3)])
-
-
-    for i in range(len(answers)):
-        if an_person1[i] == answers[i]:
+        num = answers[i]
+        if person1[i%len(person1)] == num:
             cnt1 += 1
-        if an_person2[i] == answers[i]:
+        if person2[i%len(person2)] == num:
             cnt2 += 1
-        if an_person3[i] == answers[i]:
+            # print(i, num, person2[i])
+        if person3[i%len(person3)] == num:
             cnt3 += 1
-    big = max(cnt1 , cnt2, cnt3)
+        # print(cnt1, cnt2, cnt3)
+
+    big = max(cnt1, cnt2, cnt3)
+
     if cnt1 == big:
         result.append(1)
     if cnt2 == big:
