7834cdc0fd78a237d54dc522223d51e1eb78ab19
Seoyun0626 <104416283+Seoyun0626@users.noreply.github.com>
2023-09-15 09:48:34 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,16 +1,12 @@
 def solution(numbers):
-    # 숫자를 비교할 때 기준은 문자열 *3 왜냐? 310보다 3이 더 크게 작용 -> 자리 수 맞추기
-
+    # 문자열 비교 -> 사전 순서처럼
     # numbers = [6, 10, 2]
+    # numbers = [3, 30, 34, 5, 9]
+
     answer = ""
     tmp = list(map(str, numbers))
+    tmp.sort(key=lambda x : x * 3, reverse=True)
     # print(tmp)
 
-    tmp.sort(key = lambda x : x * 3, reverse = True)
-    # print(tmp)
-
-    answer = "".join(tmp)
-    answer = int(answer) #[0,0,0] 일때 000출력 방지
-    answer = str(answer)
-
+    answer =str(int("".join(tmp)))
     return answer
