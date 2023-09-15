d23350164b1c8c5c7952465de9f03eb8e8caf87d
Seoyun0626 <104416283+Seoyun0626@users.noreply.github.com>
2023-09-15 16:33:33 +0900

    [level 2] Title: 타겟 넘버, Time: 2.81 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,18 @@
+def solution(numbers, target):
+        # numbers = [1, 1, 1, 1, 1]
+    # target = 3
+    answer = 0
+    result = 0
+
+    def dfs(i, result):
+        if i == len(numbers):
+            if result == target:
+                nonlocal answer
+                answer += 1
+            else:
+                return
+        else:
+            dfs(i + 1, result + numbers[i])
+            dfs(i + 1, result - numbers[i])
+    dfs(0,0)
+    return answer
